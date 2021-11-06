391f0b7ca0572720ca602ee23ba4608cae7dc564
admin <soinproduction@gmail.com>
2021-11-06 18:46:59 +0300

    add project

---
+++ source/js/common.js
@@ -0,0 +1,156 @@
+
+
+
+function addClassToHeader(){   // ----  функция липкой шапки   -----
+  if (document.documentElement.clientWidth > 992) {
+    window.onscroll = function() {fixedHeader()}
+    function fixedHeader() {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        header.classList.add('fixed');
+      } else {
+        header.classList.remove('fixed');
+      }
+    }
+  };
+}
+
+// window.addEventListener('resize', () => {
+//   addClassToHeader();
+// });
+// window.addEventListener('DOMContentLoaded', () => {
+//   addClassToHeader();
+// });
+
+// for (const anchor of anchors) {
+//   anchor.addEventListener('click', function (e) {
+//     document.body.classList.remove('body-fixed');
+//     mobileMenu.classList.remove('active');
+//     buttons.forEach(button => button.classList.remove('active'));
+//     e.preventDefault();
+//     const blockID = anchor.getAttribute('data-href');
+//     let sections =  document.querySelectorAll('.' + blockID);
+//     sections.forEach(function(section) {
+//       section.scrollIntoView({
+//         behavior: 'smooth',
+//         block: 'start'
+//       });
+//     });
+//   });
+// };
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   // ----  инициализация анимаций  -----
+
+
+//   if (document.documentElement.clientWidth < 1200) {
+
+//   }
+// });
+
+// --------------------------------------------------------------------
+// ----  акордион  -----
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+// ----  акордион  -----
+// --------------------------------------------------------------------
+// ---- переключение страниц -----
+
+function openPage(evt, pageName) {
+  evt.preventDefault()
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("nav-buttons__button");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(pageName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+  mobileMenu.classList.remove('active');
+  buttons.forEach(button => button.classList.remove('active'));
+  document.body.classList.remove('body-fixed');
+  addAnimate();
+
+  function addAnimate(){
+    caseSec.classList.add('activeAnimate');
+  }
+
+}
+
+// ---- переключение страниц -----
+// --------------------------------------------------------------------
+
+// --------------------------------------------------------------------
+
+// --------------------------------------------------------------------
+// ---- скролл к верху страницы -----
+let timeOut;
+function goUp() {
+   let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+   if(top > 0) {
+      window.scrollBy(0,-100);
+      timeOut = setTimeout('goUp()',20);
+   } else clearTimeout(timeOut);
+}
+
+
+
+// -----------------  Селект  --------------------
+
+const select = document.querySelectorAll(".select");
+
+if (select.length) {
+  select.forEach((item) => {
+    // достаем из текущей сущности .select__current
+    const selectCurrent = item.querySelector(".select__current");
+
+    item.addEventListener("click", (event) => {
+      const el = event.target.dataset.choice;
+      const text = event.target.innerText;
+
+      // Проверяем является ли это choosen и не выбрано ли его значение уже
+      if (el === "choosen" && selectCurrent.innerText !== text) {
+        selectCurrent.innerText = text;
+      }
+
+      item.classList.toggle("is-active");
+    });
+  });
+}
+
+
+lightGallery(document.getElementById('lightgallery'));
+lightGallery(document.getElementById('cert'));
+
+let masterSlider = new Swiper(".master-sec__slider", {
+  spaceBetween: 15,
+  slidesPerView: 3,
+  pagination: {
+        el: ".master-sec__dots",
+        clickable: true,
+      },
+  navigation: {
+    nextEl: ".master-sec__next",
+    prevEl: ".master-sec__prev",
+  },
+});
